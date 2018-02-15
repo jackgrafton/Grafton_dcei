346e39c85b2acf5b0a243d5e71965e8f3b33a086
Jack Grafton <JackGrafton@Jacks-MacBook-Air-4.local>
2018-02-15 10:29:48 -0600

    adding a file for learning how to add a file

---
+++ bootcamp1.R
@@ -0,0 +1,45 @@
+wing.length.cm <- c(1.4,5.6,9.9)
+wing.length.cm <- c(1.4,5.6,9.9)
+wing.length.cm <- c(1.4,5.6,9.9)
+
+bodyMass.kg <- c(10,1.2,2.12,1001)
+# comment
+years
+
+answer <- 7
+paste("The answer is",answer)
+
+plot((1:25), (1:25)^2, main="A simple graph",xlab="x",ylab="y",cex.main=2,pch=15,col.main=2,
+     col=3,type="l")
+lines(1:25, (25:1)^2)
+
+for(year in 2009:2006){
+  print(paste("The year is", year))
+}
+
+ten.factorial <- 1
+for(counter in 1:6){
+  ten.factorial <- ten.factorial * counter
+  print(ten.factorial)
+}
+
+for(counter in 1:5){
+  print(paste("Dog"))
+}
+
+for(i in 50:40){
+  print(i)
+}
+
+for(i in 1:12){
+print(month.name[i])
+}
+
+for(counter in 1:10){
+  if (counter>5){
+   print("Hello") }
+  
+  if (counter<5){
+   print("Goodbye") }
+}
+
